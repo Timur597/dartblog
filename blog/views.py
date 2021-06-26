5395bb00e8b7144cb6c1fcc7667b1330297678d0
Timur <khalilov.timur97@mail.ru>
2021-06-26 14:12:36 +0600

    1

---
+++ blog/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
+from django.db.models import F
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Мой первый блог'
+        return context
+
+
+class PostByTag(ListView):
+    pass
+
+
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
+        return context
+
+
+# def index(request):
+#     return render(request, 'blog/index.html')
+#
+#
+# def get_category(request, slug):
+#     return render(request, 'blog/category.html')
+#
+#
+# def get_post(request, slug):
+#     return render(request, 'blog/category.html')
